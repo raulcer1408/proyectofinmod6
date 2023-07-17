67fa28bffebf4c1b40d1b32a257ab1078336f4a3
raul cervantes <raulcer.rmc@gmail.com>
2023-07-17 18:09:49 -0400

    agregando axios e importandolo

--- vue3/cursos/src/main.js
+++ vue3/cursos/src/main.js
@@ -4,4 +4,6 @@ import router from './router'
 import store from './store'
 import 'materialize-css/dist/css/materialize.min.css';
 import 'material-design-icons-iconfont/dist/material-design-icons.min.css';
-createApp(App).use(store).use(router).mount('#app')
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+createApp(App).use(store).use(router).use(VueAxios,axios).mount('#app')
