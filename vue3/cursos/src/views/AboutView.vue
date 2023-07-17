17deaee6f42786a2f6c88ea666dd341390e7c47c
raul cervantes <raulcer.rmc@gmail.com>
2023-07-17 18:06:44 -0400

    instalando axios

--- vue3/cursos/src/views/AboutView.vue
+++ vue3/cursos/src/views/AboutView.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <h1>Esta pagina es donde se hace las pruebas</h1>
+    <button class="btn waves-effect waves-light" type="submit" name="action">Guardar
+    <i class="material-icons right">save</i>
+  </button>
+     </div>
 </template>
